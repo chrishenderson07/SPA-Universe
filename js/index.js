6b3132d58abccd85bd2430744da67f2dc24b6730
nerodesigndev <chris.hend012@gmail.com>
2022-09-30 07:08:15 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+import Router from './routes.js'
+import { backgroundChange } from './backgroundChange.js'
+
+const navStyle1 = document.querySelector('nav :nth-child(2)')
+const navStyle2 = document.querySelector('nav :nth-child(3)')
+const navStyle3 = document.querySelector('nav :nth-child(4)')
+const bodyPage = document.querySelector('body')
+
+const router = new Router()
+const backgroundChanges = backgroundChange({ bodyPage })
+
+router.add('/', './pages/home.html')
+router.add('/home', './pages/home.html')
+router.add('/universe', './pages/universe.html')
+router.add('/explore', './pages/explore.html')
+router.add(404, './pages/404.html')
+
+router.handle()
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
+
+navStyle1.addEventListener('click', backgroundChanges.home)
+navStyle2.addEventListener('click', backgroundChanges.universe)
+navStyle3.addEventListener('click', backgroundChanges.exploration)
---
+++ readme.md
@@ -0,0 +1,23 @@
+<h1 align="center">SPA Universe 🖖</h1>
+
+<p align="center">Projeto SPA (Single Page Application) construido para exercitar conceitos sobre:</p>
+
+<ul>
+  <li>SPA (Single page Application</li>
+  <li>Assincronismo</li>
+  <li>Class e Orientanção a objetos.</li>
+  <li>Mapeamento de rotas</li>
+</ul>
+
+<h2 align="center">Tecnologias utilizadas</h2>
+
+<p align="center">
+  <a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=html,css,js" />
+  </a>
+</p>
+
+<a href="https://chrishenderson07.github.io/focus-timer-dark-mode/"><h2 align="center">🔗Abrir e Rodar o projeto🔗</h2>
+
+![Imagem de capa do Timer Focus](./images/capa.png)
+</a>
